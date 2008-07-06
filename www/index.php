83430968d364ac7107d22cb97feaa117c5bbd1cf
Yihui Xie <xie@yihui.name>
2008-07-06 10:14:46 +0000

    homepage revised

--- www/index.php
+++ www/index.php
@@ -3,11 +3,20 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Animations in Statistics</title>
+<style type="text/css">
+<!--
+body {
+	font-family: "Bitstream Charter", Georgia, "Times New Roman", serif;
+	font-size: 14px;
+	line-height: 20px;
+}
+-->
+</style>
 </head>
 
 <body>
-<p>&nbsp;</p>
-<p align="center"><a href="http://animation.yihui.name/"><img src="http://animation.yihui.name/_media/ani_frontpage.gif?cache=cache&amp;w=400&amp;h=400" alt="Gradient Descent Algorithm" width="400" height="400" border="0" /></a></p>
-<p></p>
+<p>Welcome to the <strong>animation</strong> project! This web is still under construction; for further information, please visit <a href="http://animation.yihui.name" target="_blank"><strong>AniWiki</strong> (http://animation.yihui.name)</a>. </p>
+<p align="center"><a href="http://animation.yihui.name/"><img src="http://animation.yihui.name/_media/ani_frontpage.gif" alt="Gradient Descent Algorithm" width="400" height="400" border="0" /></a></p>
+<p align="center"><a href="http://www.yihui.name" target="_blank">Yihui Xie</a> @ Jul 6, 2008 </p>
 </body>
 </html>
