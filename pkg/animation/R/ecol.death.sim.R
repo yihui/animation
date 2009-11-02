be326a0029d4b7bcd5fbe44c6f252c42de531a89
Yihui Xie <xie@yihui.name>
2009-11-02 16:02:32 +0000

    new simulation function ecol.death.sim(); fixed the example for HuSpeech data

---
+++ pkg/animation/R/ecol.death.sim.R
@@ -0,0 +1,25 @@
+ecol.death.sim <-
+function(nr = 10, nc = 10, col.sp = c(1, 
+    2), pch.sp = c(1, 2), col.die = 1, pch.die = 4, cex = 3) {
+    x = rep(1:nc, nr)
+    y = rep(1:nr, each = nc)
+    p = sample(rep(1:2, nr * nc/2), nr * nc)
+    nmax = ani.options("nmax")
+    interval = ani.options("interval")
+    for (i in 1:nmax) {
+        plot(1:nc, 1:nr, type = "n", xlim = c(0.5, nc + 0.5), 
+            ylim = c(0.5, nr + 0.5))
+        abline(h = 1:nr, v = 1:nc, col = "lightgray", lty = 3)
+        points(x, y, col = col.sp[p], pch = pch.sp[p], cex = cex)
+        Sys.sleep(interval)
+        idx = sample(nr * nc, 1)
+        points(x[idx], y[idx], pch = pch.die, col = col.die, 
+            cex = cex, lwd = 3)
+        tbl = as.vector(table(p))
+        tbl = tbl + sign(p[idx] - 1.5) * c(1, -1)
+        p[idx] = sample(1:2, 1, prob = tbl)
+        Sys.sleep(interval)
+    }
+    p
+}
+
