fe6a699bacb30dc74d14b661ba541b89edf2287e
Yihui Xie <xie@yihui.name>
2009-11-03 01:31:34 +0000

    we can specify number of plants for the two species in ecol.death.sim()

--- pkg/animation/R/ecol.death.sim.R
+++ pkg/animation/R/ecol.death.sim.R
@@ -1,14 +1,14 @@
 ecol.death.sim <-
-function(nr = 10, nc = 10, col.sp = c(1, 
-    2), pch.sp = c(1, 2), col.die = 1, pch.die = 4, cex = 3) {
+function(nr = 10, nc = 10, num.sp = c(50, 50), col.sp = c(1, 
+    2), pch.sp = c(1, 2), col.die = 1, pch.die = 4, cex = 3, ...) {
     x = rep(1:nc, nr)
     y = rep(1:nr, each = nc)
-    p = sample(rep(1:2, nr * nc/2), nr * nc)
+    p = factor(sample(rep(1:2, num.sp)), levels = 1:2)
     nmax = ani.options("nmax")
     interval = ani.options("interval")
     for (i in 1:nmax) {
         plot(1:nc, 1:nr, type = "n", xlim = c(0.5, nc + 0.5), 
-            ylim = c(0.5, nr + 0.5))
+            ylim = c(0.5, nr + 0.5), ...)
         abline(h = 1:nr, v = 1:nc, col = "lightgray", lty = 3)
         points(x, y, col = col.sp[p], pch = pch.sp[p], cex = cex)
         Sys.sleep(interval)
@@ -16,10 +16,10 @@ function(nr = 10, nc = 10, col.sp = c(1,
         points(x[idx], y[idx], pch = pch.die, col = col.die, 
             cex = cex, lwd = 3)
         tbl = as.vector(table(p))
-        tbl = tbl + sign(p[idx] - 1.5) * c(1, -1)
+        tbl = tbl + if (as.integer(p[idx]) > 1) c(0, -1) else c(-1, 0)
         p[idx] = sample(1:2, 1, prob = tbl)
         Sys.sleep(interval)
     }
-    p
+    invisible(p)
 }
 
