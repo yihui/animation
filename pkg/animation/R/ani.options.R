a51c792f5cd80656117b5f2506578c85a5a14275
Yihui Xie <xie@yihui.name>
2009-11-08 06:01:25 +0000

    set interval =0 when R is not run interactively; option example.ask = FALSE

--- pkg/animation/R/ani.options.R
+++ pkg/animation/R/ani.options.R
@@ -1,23 +1,26 @@
-`ani.options` <- function(...) {
-    mf = list(interval = 1, nmax = 50, ani.width = 480,
-        ani.height = 480, outdir = tempdir(), filename = "index.htm",
-        withprompt = "ANI> ", ani.type = "png", ani.dev = png,
-        title = "Statistical Animations Using R", description = "This is an animation.",
-        footer = TRUE, loop = TRUE, autobrowse = TRUE)
-    if (is.null(getOption("ani")))
+ani.options <- function(...) {
+    mf = list(interval = 1, nmax = 50, ani.width = 480, ani.height = 480, 
+        outdir = tempdir(), filename = "index.htm", withprompt = "ANI> ", 
+        ani.type = "png", ani.dev = png, title = "Statistical Animations Using R", 
+        description = "This is an animation.", footer = TRUE, 
+        loop = TRUE, autobrowse = TRUE)
+    if (is.null(getOption("ani"))) 
         options(ani = mf)
     else mf = getOption("ani")
     lst = list(...)
     if (length(lst)) {
         if (is.null(names(lst)) & !is.list(lst[[1]])) {
-            getOption("ani")[unlist(lst)][[1]]
+            if (identical(unlist(lst), "interval") & !interactive()) 
+                return(0)
+            else return(getOption("ani")[unlist(lst)][[1]])
+            
         }
         else {
             omf = mf
             mc = list(...)
-            if (is.list(mc[[1]]))
+            if (is.list(mc[[1]])) 
                 mc = mc[[1]]
-            if (length(mc) > 0)
+            if (length(mc) > 0) 
                 mf[pmatch(names(mc), names(mf))] = mc
             options(ani = mf)
             return(invisible(omf))
@@ -26,4 +29,4 @@
     else {
         getOption("ani")
     }
-}
+} 
