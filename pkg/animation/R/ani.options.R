ca19e9fffda3975ee02dbdef73ddccc4f49df879
Yihui Xie <xie@yihui.name>
2009-11-09 21:49:34 +0000

    removed animation option 'msg'

--- pkg/animation/R/ani.options.R
+++ pkg/animation/R/ani.options.R
@@ -3,17 +3,16 @@ ani.options <- function(...) {
         outdir = tempdir(), imgdir = "images", filename = "index.htm", 
         withprompt = "ANI> ", ani.type = "png", ani.dev = "png", 
         title = "Statistical Animations Using R", description = "This is an animation.", 
-        footer = TRUE, loop = TRUE, autobrowse = TRUE, msg = interactive())
+        footer = TRUE, loop = TRUE, autobrowse = TRUE)
     if (is.null(getOption("ani"))) 
         options(ani = mf)
     else mf = getOption("ani")
-    oomf = mf
     lst = list(...)
     if (length(lst)) {
         if (is.null(names(lst)) & !is.list(lst[[1]])) {
             if (identical(unlist(lst), "interval") & !interactive()) 
-                res = 0
-            else res = getOption("ani")[unlist(lst)][[1]]
+                0
+            else getOption("ani")[unlist(lst)][[1]]
             
         }
         else {
@@ -24,38 +23,13 @@ ani.options <- function(...) {
             if (length(mc) > 0) 
                 mf[pmatch(names(mc), names(mf))] = mc
             options(ani = mf)
-            res = omf
+            if (!identical(omf$nmax, mf$nmax) & interactive()) {
+                message("animation option 'nmax' changed: ", omf$nmax, "--> ", mf$nmax)
+            }
+            invisible(omf)
         }
     }
     else {
-        res = getOption("ani")
+        getOption("ani")
     }
-    nnmf = getOption("ani")
-    if (isTRUE(nnmf$msg)) {
-        if (!identical(oomf, nnmf)) {
-            if (is.function(nnmf$ani.dev) | is.function(oomf$ani.dev)) {
-                nnmf$ani.dev = NULL
-                oomf$ani.dev = NULL
-            }
-            for (i in names(oomf)) {
-                if (!identical(oomf[[i]], nnmf[[i]])) {
-                  if (!(i %in% c("interval", "outdir"))) {
-                    message("option '", i, "' changed: ", oomf[[i]], 
-                      " --> ", nnmf[[i]])
-                  }
-                  else {
-                    if ((i == "interval") & length(nnmf$interval) == 
-                      1) 
-                      message("option '", i, "' changed: ", oomf[[i]], 
-                        " --> ", nnmf[[i]])
-                    if ((i == "outdir") & length(nnmf$outdir) == 
-                      1) 
-                      message("option '", i, "' changed: ", oomf[[i]], 
-                        " --> ", nnmf[[i]])
-                  }
-                }
-            }
-        }
-    }
-    invisible(res)
 } 
