4f20bdfd7923df72dbd65d767631be41d6e20369
Yihui Xie <xie@yihui.name>
2010-12-03 12:49:44 -0600

    ani.news() and tidy.source() were removed from the package

--- R/animation-defunct.R
+++ R/animation-defunct.R
@@ -6,19 +6,24 @@
 ##'
 ##' \code{write.rss} is irrelevant either; the source code can be found at
 ##' \url{https://github.com/yihui/animation/raw/1650902e43fe283d4fd0cea8f559e4e5187d60fe/R/write.rss.R}.
+##'
+##' \code{tidy.source} has been moved to the \pkg{formatR} package.
+##'
+##' \code{ani.news} is no longer necessary since R has got a function
+##' \code{\link[utils]{news}} in the \pkg{utils} package
 ##' @name animation-defunct
-##' @aliases animation-defunct highlight.def write.rss
+##' @aliases animation-defunct highlight.def write.rss tidy.source ani.news
 ##' @author Yihui Xie <\url{http://yihui.name}>
 ##' @seealso \code{\link[base]{Defunct}}
 ##' @keywords internal
 ##'
 NULL
 
-highlight.def <- function(file = "r.lang") {
-    .Defunct('http://yihui.name/en/2007/09/r-language-definition-file-for-highlight/')
+highlight.def = function(file = "r.lang") {
+    .Defunct('http://yihui.name/en/2007/09/r-language-definition-file-for-highlight/', 'animation')
 }
 
-write.rss <- function(file = "feed.xml", entry = "rss.csv",
+write.rss = function(file = "feed.xml", entry = "rss.csv",
     xmlver = "1.0", rssver = "2.0", title = "What's New?",
     link = "http://yihui.name",
     description = "A Website", language = "en-us",
@@ -28,5 +33,15 @@ write.rss <- function(file = "feed.xml", entry = "rss.csv",
     managingEditor = "xie@yihui.name",
     webMaster = "xie@yihui.name",
     maxitem = 10, ...) {
-    .Defunct('https://github.com/yihui/animation/raw/1650902e43fe283d4fd0cea8f559e4e5187d60fe/R/write.rss.R')
+    .Defunct('https://github.com/yihui/animation/raw/1650902e43fe283d4fd0cea8f559e4e5187d60fe/R/write.rss.R', 'animation')
+}
+
+tidy.source = function(source = "clipboard", keep.comment = TRUE,
+    keep.blank.line = TRUE, begin.comment, end.comment, output = TRUE,
+    width.cutoff = 60L, ...) {
+    .Defunct('formatR::tidy.source', 'animation')
+}
+
+ani.news = function(...){
+    .Defunct('utils::news', 'animation')
 }
