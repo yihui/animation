b1aa4a997325c07877a6c7716fb8b110e010e3fe
Yihui Xie <xie@yihui.name>
2010-12-30 17:47:05 -0600

    use the Cairo device with this package; a new demo

--- R/ani.options.R
+++ R/ani.options.R
@@ -67,8 +67,8 @@
 ##' devices -- a typical example is the \pkg{rgl} graphics (we can use
 ##' \code{\link[rgl]{rgl.snapshot}} to capture \pkg{rgl} graphics to
 ##' png files, or \code{\link[rgl]{rgl.postscript}} to save plots as
-##' postscript/pdf; see \code{demo('rgl_animation')} for an example or
-##' the last example below)}
+##' postscript/pdf; see \code{demo('rgl_animation')} or
+##' \code{demo('use_Cairo')} for examples or the last example below)}
 ##'
 ##' \item{withprompt}{character: prompt to display while using
 ##' \code{\link{ani.start}} (will be restored with
