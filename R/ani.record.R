ec072d67f89da71b0215ff5f7bd7c8d58b4bc3d3
Yihui <yihui@xie.(none)>
2011-01-13 22:49:31 -0600

    a new option 'replay.cur' for ani.record()

--- R/ani.record.R
+++ R/ani.record.R
@@ -12,12 +12,19 @@
 ##' @param reset if \code{TRUE}, the recording list will be cleared,
 ##' otherwise new plots will be appended to the existing list of recorded
 ##' plots
+##' @param replay.cur whether to replay the current plot (we can set
+##' both \code{reset} and \code{replay.cur} to \code{TRUE} so that
+##' low-level plotting changes can be captured by off-screen graphics
+##' devices without storing all the plots in memory; see Note)
 ##' @return Invisible \code{NULL}.
 ##' @note Although we can record changes made by low-level plotting
 ##' commands using \code{\link{ani.record}}, there is a price to pay
 ##' -- we need memory to store the recorded plots, which are usually
 ##' verg large when the plots are complicated (e.g. we draw millions
-##' of points or polygons in a single plot).
+##' of points or polygons in a single plot). However, we can set
+##' \code{replay.cur} to force R to produce a new copy of the current
+##' plot, which will be automatically recorded by off-screen grapihcs
+##' devices as \emph{new} image files.
 ##'
 ##' If we use base graphics, we should bear in mind that the
 ##' background colors of the plots might be transparent, which could
@@ -52,7 +59,7 @@
 ##'
 ##' ani.options(oopts)
 ##'
-ani.record = function(reset = FALSE) {
+ani.record = function(reset = FALSE, replay.cur = FALSE) {
     if (reset) .ani.env$.images = list() else {
         ## make sure a graphics device has been opened
         if (dev.cur() != 1) {
@@ -60,6 +67,7 @@ ani.record = function(reset = FALSE) {
             .ani.env$.images[[n + 1]] = recordPlot()
         } else warning('no current device to record from')
     }
+    if (replay.cur && dev.cur() != 1) replayPlot(recordPlot())
     invisible(NULL)
 }
 
