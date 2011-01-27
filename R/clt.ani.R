478a5b9da79caa3b719901481508a30bf6bd9144
Yihui Xie <xie@yihui.name>
2011-01-27 01:03:44 -0600

    new argument for clt.ani()

--- R/clt.ani.R
+++ R/clt.ani.R
@@ -24,6 +24,8 @@
 ##' @param mat,widths,heights arguments passed to
 ##' \code{\link[graphics]{layout}} to set the layout of the two
 ##' graphs.
+##' @param xlim the x-axis limit for the histogram (it has a default
+##' value if not specified)
 ##' @param \dots other arguments passed to \code{\link[graphics]{plot.default}}
 ##' to plot the P-values
 ##' @return None.
@@ -60,16 +62,17 @@
 ##'
 clt.ani = function(obs = 300, FUN = rexp, col = c("bisque",
     "red", "black"), mat = matrix(1:2, 2), widths = rep(1, ncol(mat)),
-    heights = rep(1, nrow(mat)), ...) {
+    heights = rep(1, nrow(mat)), xlim, ...) {
     nmax = ani.options("nmax")
     x = matrix(nrow = nmax, ncol = obs)
     for (i in 1:nmax) x[i, ] = apply(matrix(replicate(obs,
         FUN(i)), i), 2, mean)
     pvalue = apply(x, 1, function(xx) shapiro.test(xx)$p.value)
     layout(mat, widths, heights)
+    if (missing(xlim)) xlim = quantile(x, c(.005, .995))
     for (i in 1:nmax) {
         hist(x[i, ], freq = FALSE, main = "", xlab = substitute(italic(bar(x)[i]),
-            list(i = i)), col = col[1])
+            list(i = i)), col = col[1], xlim = xlim)
         lines(density(x[i, ]), col = col[2])
         legend("topright", legend = paste("P-value:", format(round(pvalue[i],
             3), nsmall = 3)), bty = "n")
