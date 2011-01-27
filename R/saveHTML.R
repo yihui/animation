a7d73267d360408af394a10c7acef419644d5845
Yihui Xie <xie@yihui.name>
2011-01-26 18:59:58 -0600

    a right bracket missing

--- R/saveHTML.R
+++ R/saveHTML.R
@@ -152,7 +152,7 @@ saveHTML = function(expr, img.name = 'Rplot',
                                          ani.options('title')), html)
     html = sub('<meta name="generator" content=".*">',
               sprintf('<meta name="generator" content="R package animation %s">',
-                      packageDescription('animation', fields = 'Version'), html)
+                      packageDescription('animation', fields = 'Version')), html)
     div.str = sprintf('	<div class="scianimator"><div id="%s" style="display: inline-block;"></div></div>', img.name0)
     js.str = sprintf('	<script src="js/%s.js"></script>', img.name)
     n = grep('<!-- highlight R code -->', html, fixed = TRUE)
