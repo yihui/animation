88903e7ecd6746e04e288f8c527a12a7801f7a61
Yihui Xie <xie@yihui.name>
2011-11-19 15:39:44 -0600

    ani.pause() also flushes the device now

--- R/ani.pause.R
+++ R/ani.pause.R
@@ -1,13 +1,14 @@
-##' Pause for a while
+##' Pause for a while and flush the current graphical device
 ##'
 ##' If this function is called in an interactive graphics device, it
 ##' will pause for a time interval (specified in
-##' \code{\link{ani.options}('interval')}); otherwise it will do
-##' nothing.
+##' \code{\link{ani.options}('interval')}) and flush the current
+##' device; otherwise it will do nothing.
 ##'
 ##' @return Invisible \code{NULL}.
 ##' @author Yihui Xie <\url{http://yihui.name}>
-##' @seealso \code{\link[grDevices]{dev.interactive}}, \code{\link[base]{Sys.sleep}}
+##' @seealso \code{\link[grDevices]{dev.interactive}},
+##' \code{\link[base]{Sys.sleep}}, \code{\link[grDevices]{dev.flush}}
 ##' @examples ## pause for 2 seconds
 ##' oopt = ani.options(interval = 2)
 ##'
@@ -21,5 +22,8 @@
 ##' ## see demo('Xmas2', package = 'animation') for another example
 ##'
 ani.pause = function() {
-    if (dev.interactive()) Sys.sleep(ani.options('interval'))
+    if (dev.interactive()) {
+        dev.flush()
+        Sys.sleep(ani.options('interval'))
+    }
 }
