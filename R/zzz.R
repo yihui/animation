253d0f6a47bbfd237f70a3f9079d3f23de3d3bd2
Yihui Xie <xie@yihui.name>
2012-06-20 00:40:15 -0400

    cosmetic changes in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,14 @@
-.onLoad <- function(lib, pkg) {
-    options(demo.ask = FALSE, example.ask = FALSE)
-    .ani.env$.ani.opts =
-        list(interval = 1, nmax = 50, ani.width = 480, ani.height = 480,
-             outdir = tempdir(), imgdir = "images", htmlfile = "index.html",
-             withprompt = "ANI> ", ani.type = "png", ani.dev = "png",
-             title = "Animations Using the R Language",
-             description = paste("Animations generated in", R.version.string,
-             'using the package animation'),
-             verbose = TRUE, loop = TRUE, autobrowse = interactive(),
-             autoplay = TRUE, use.dev = TRUE)
-    .ani.env$check = 'CheckExEnv' %in% search() # am I in a check process?
+.onLoad = function(lib, pkg) {
+  options(demo.ask = FALSE, example.ask = FALSE)
+  .ani.env$.ani.opts = list(
+    interval = 1, nmax = 50, ani.width = 480, ani.height = 480,
+    outdir = tempdir(), imgdir = "images", htmlfile = "index.html",
+    withprompt = "ANI> ", ani.type = "png", ani.dev = "png",
+    title = "Animations Using the R Language",
+    description = paste("Animations generated in", R.version.string,
+                        'using the package animation'),
+    verbose = TRUE, loop = TRUE, autobrowse = interactive(),
+    autoplay = TRUE, use.dev = TRUE
+  )
+  .ani.env$check = 'CheckExEnv' %in% search() # am I in a check process?
 }
