af194bbd196d560e64a7d80183148985c7282881
Yihui Xie <xie@yihui.name>
2013-06-24 16:39:16 -0700

    mark ani.start() and ani.stop() as internal

--- R/ani.start.R
+++ R/ani.start.R
@@ -3,6 +3,7 @@
 #' These functions are defunct and should not be used any more.
 #' @param ... arguments passed to \code{\link{ani.options}} to set
 #' animation parameters
+#' @keywords internal
 ani.start = function(...) {
   .Defunct(msg = 'This function is defunct. Use animation::saveHTML() instead.')
 }
