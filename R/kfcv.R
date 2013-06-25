663e28c8616c92460be82c4faf4a1870994f73ba
Yihui Xie <xie@yihui.name>
2013-06-24 18:04:26 -0700

    cosmetic

--- R/kfcv.R
+++ R/kfcv.R
@@ -2,14 +2,12 @@
 #'
 #' Compute sample sizes for \eqn{k}-fold cross-validation.
 #'
-#' If N/k is an integer, the sample sizes are k `N/k's (N/k, N/k,
-#' ...), otherwise the remainder will be allocated to each group as
-#' `uniformly' as possible, and at last these sample sizes will be
-#' permuted randomly.
+#' If N/k is an integer, the sample sizes are k `N/k's (N/k, N/k, ...),
+#' otherwise the remainder will be allocated to each group as `uniformly' as
+#' possible, and at last these sample sizes will be permuted randomly.
 #' @param k number of groups.
 #' @param N total sample size.
-#' @return A vector of length \code{k} containing \eqn{k} sample
-#' sizes.
+#' @return A vector of length \code{k} containing \eqn{k} sample sizes.
 #' @author Yihui Xie <\url{http://yihui.name}>
 #' @seealso \code{\link{cv.ani}}
 #' @export
@@ -20,16 +18,14 @@
 #' ## not divisible
 #' kfcv(10,77)
 kfcv <- function(k, N) {
-    if (k > N) {
-        warning("'k' is larger than 'N'!")
-        return(rep(1, N))
+  if (k > N) {
+    warning("'k' is larger than 'N'!")
+    return(rep(1, N))
+  } else {
+    if (N%%k == 0) {
+      rep(N%/%k, k)
+    } else {
+      sample(c(rep(1, N%%k), rep(0, k - N%%k)) + rep(N%/%k, k))
     }
-    else {
-        if (N%%k == 0) {
-            rep(N%/%k, k)
-        }
-        else {
-            sample(c(rep(1, N%%k), rep(0, k - N%%k)) + rep(N%/%k, k))
-        }
-    }
+  }
 }
