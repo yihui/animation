1f7fe6e2736345b2d77567f82053bd1c18b0f39b
Yihui Xie <xie@yihui.name>
2013-11-02 17:25:55 -0500

    cosmetic

--- R/quincunx2.R
+++ R/quincunx2.R
@@ -1,11 +1,11 @@
 #' @rdname quincunx
 #' @export
 
-quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, col.balls = sample(colors(), 
+quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, col.balls = sample(colors(),
     balls, TRUE), cex.balls = 2) {
-    
+
     op = par(mar = c(1, 0.1, 0.1, 0.1), mfcol = c(4, 1)); on.exit(par(op))
-    if (ani.options("nmax") != (balls + layers - 2)) 
+    if (ani.options("nmax") != (balls + layers - 2))
         warning("It's strongly recommended that ani.options(nmax = balls + layers -2)")
     nmax = max(balls + layers - 2, ani.options("nmax"))
     layerx = layery = newlayerx = newlayery = NULL
@@ -23,12 +23,12 @@ quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, c
             newlayery = c(newlayery, rep(i, layers - newi + 1))
         }
     }
-    
-    
-    
+
+
+
     ballx = bally = matrix(nrow = balls, ncol = nmax)
     finalx = newfinalx = numeric(balls)
-    
+
     for (i in 1:balls) {
         ballx[i, i] = (1 + layers)/2
         if (layers > 2) {
@@ -38,14 +38,14 @@ quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, c
         bally[i, (i - 1) + 1:(layers - 1)] = (layers - 1):1
         finalx[i] = ballx[i, i + layers - 2]
     }
-    
+
     rgx = c(1, layers)
     rgy = c(0, max(table(finalx)))
-    
-    
+
+
     newballx = ballx
     diag(newballx) = finalx
-    
+
     for (i in 1:balls) {
         tmp = rbinom(layers - 2, 1, 0.5) * 2 - 1
         tmp = ifelse(newballx[i, i] + cumsum(tmp) < rgx[2], tmp, -1)
@@ -53,8 +53,8 @@ quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, c
         newballx[i, i + 1:(layers - 2)] = newballx[i, i] + cumsum(tmp) * 0.5
         newfinalx[i] = newballx[i, i + layers - 2]
     }
-    
-    
+
+
     for (i in 1:ani.options("nmax")) {
         dev.hold()
         plot(1:layers, type = "n", ann = FALSE, axes = FALSE)
@@ -64,11 +64,11 @@ quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, c
         if (i < layers - 1) {
             plot.new()
         } else {
-            hist(finalx[1:(i - layers + 2)], breaks = 1:layers, xlim = rgx, ylim = rgy, main = "", xlab = "", 
+            hist(finalx[1:(i - layers + 2)], breaks = 1:layers, xlim = rgx, ylim = rgy, main = "", xlab = "",
                 ylab = "", ann = FALSE, axes = FALSE)
-            
+
         }
-        
+
         if (i > (layers - 1)) {
             newi = i - layers + 1
             plot(1:layers, type = "n", ann = FALSE, axes = FALSE)
@@ -78,11 +78,11 @@ quincunx2 = function(balls = 200, layers = 15, pch.layers = 2, pch.balls = 19, c
             if (newi < layers - 1) {
                 plot.new()
             } else {
-                hist(newfinalx[1:(newi - layers + 2)], breaks = 1:layers, xlim = rgx, ylim = rgy, main = "", 
+                hist(newfinalx[1:(newi - layers + 2)], breaks = 1:layers, xlim = rgx, ylim = rgy, main = "",
                   xlab = "", ylab = "", ann = FALSE, axes = FALSE)
-                
+
             }
-            
+
         } else {
             plot(1:layers, type = "n", ann = FALSE, axes = FALSE)
             points(newlayerx, newlayery, pch = pch.layers)
