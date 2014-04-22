cc4bac93368912d5e37245b1e02c95b4089a0ba8
Barry Rowlingson <b.rowlingson@gmail.com>
2014-04-22 16:51:15 +0100

    docs;passes check

--- R/saveHTML2.R
+++ R/saveHTML2.R
@@ -1,17 +1,13 @@
-#' Insert animations into an HTML page
+#' Insert animations into an HTML page, AngularJS version
 #'
 #' This function first records all the plots in the R expression as bitmap
 #' images, then inserts them into an HTML page and finally creates the animation
-#' using a template that uses ANgularJS
+#' using a template that uses AngularJS and Twitter Bootstrap. You will need to
+#' be connected to the internet as the page gets these things from remove servers.
 #'
-#' Optionally the source code and some session information can be added below
-#' the animations for the sake of reproducibility (specified by the option
-#' \code{ani.options('verbose')} -- if \code{TRUE}, the description, loaded
-#' packages, the code to produce the animation, as well as a part of
-#' \code{\link{sessionInfo}()} will be written in the bottom of the animation;
-#' the R code will be highlighted using the SyntaxHighlighter library for better
-#' reading experience).
-#'
+#' Two sliders are displayed, one sets the frame and the other controls the speed.
+#' A drop-down control sets the looping mode.
+#' 
 #' @param expr an R expression to be evaluated to create a sequence of images
 #' @param img.name the filename of the images (the real output will be like
 #'   \file{img.name1.png}, \file{img.name2.png}, ...); this name has to be
@@ -21,47 +17,27 @@
 #'   make it a legal jQuery string:
 #'
 #'   \verb{!"#$\%&'()*+,./:;?@@[\]^`{|}~}
-#' @param global.opts a string: the global options of the animation; e.g. we can
-#'   specify the default theme to be blue using
-#'   \verb{$.fn.scianimator.defaults.theme = 'blue';} note these options must be
-#'   legal JavaScript expressions (ended by \code{';'})
-#' @param single.opts the options for each single animation (if there are
-#'   multiple ones in one HTML page), e.g. to use the dark theme and text labels
-#'   for buttons:
-#'
-#'   \verb{'utf8': false, 'theme': 'dark'}
-#'
-#'   or to remove the navigator panel (the navigator can affect the smoothness
-#'   of the animation when the playing speed is extremely fast (e.g.
-#'   \code{interval} less than 0.05 seconds)):
-#'
-#'   \verb{'controls': ['first', 'previous', 'play', 'next', 'last', 'loop',
-#'   'speed']}
-#'
-#'   see the reference for a complete list of available options
+#' @param title HTML page title
 #' @param htmlfile the filename of the HTML file
+#' @param pause Time in milliseconds between frames
+#' @param endPause Time between restarting when in Loop-pause mode
+#' @param loopmode Either "One-shot", "Loop", or "Loop-pause" to describe
+#'   what to do at the end of a frame sequence. "Loop-pause" pauses for
+#'   an extra period so the end is clear. This is similar to rainfall radar
+#'   sequences often seen on weather forecasts.
+#' @param overwrite Whether to stomp on an existing HTML file
+#' @param description Some HTML to place in a container DIV after the animation.
+#'   You would typically want to make paragraphs with P tags.
 #' @param ... other arguments to be passed to \code{\link{ani.options}} to
 #'   animation options such as the time interval between image frames
 #' @return The path of the HTML output.
-#' @note Microsoft IE might restrict the HTML page from running JavaScript and
-#'   try to ``protect your security'' when you view the animation page, but this
-#'   is not really a security problem.
-#'
-#'   When you want to publish the HTML page on the web, you have  to upload the
-#'   associated \file{css} and \file{js} folders with the HTML file as well as
+#' @note When you want to publish the HTML page on the web, you only have  to upload the
+#'   the HTML file and 
 #'   the images.
 #'
-#'   For \code{\link{saveHTML}}, \code{ani.options('description')} can be a
-#'   character vector, in which case this vector will be pasted into a scalar;
-#'   use \code{'\n\n'} in the string to separate paragraphs (see the first
-#'   example below).
+#'   This was loosely based on code from the choroplethr package but has been
+#'   extensively rewritten by Barry Rowlingson
 #'
-#'   For the users who do not have R at hand, there is a demo in
-#'   \code{system.file('misc', 'Rweb', 'demo.html', package = 'animation')} to
-#'   show how to create animations online without R being installed locally. It
-#'   depends, however, on whether the Rweb service can be provided for public
-#'   use in a long period (currently we are using the Rweb at Tama University).
-#'   See the last example below.
 #' @author Yihui Xie, Barry Rowlingson
 #' @family utilities
 #' @export
