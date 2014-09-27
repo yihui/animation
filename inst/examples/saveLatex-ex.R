8e3e508d1a179e8e4bb0a433e04cda9b608d745f
Yihui Xie <xie@yihui.name>
2014-09-27 16:54:55 -0500

    fix the real problem in #53: should not edit Rd directly, but the R example in roxygen instead

--- inst/examples/saveLatex-ex.R
+++ inst/examples/saveLatex-ex.R
@@ -5,11 +5,11 @@ saveLatex({
       tcl = -0.3, cex.axis = 0.8, cex.lab = 0.8, cex.main = 1)
   brownian.motion(pch = 21, cex = 5, col = 'red', bg = 'yellow',
                   main = 'Demonstration of Brownian Motion')
-}, img.name = 'BM', ani.opts = 'controls,loop,width=0.95\\\\textwidth',
+}, img.name = 'BM', ani.opts = 'controls,loop,width=0.95\\textwidth',
           latex.filename = ifelse(interactive(), 'brownian_motion.tex', ''),
           interval = 0.1, nmax = 10,
           ani.dev = 'pdf', ani.type = 'pdf', ani.width = 7, ani.height = 7,
-          documentclass = paste('\\\\documentclass{article}',
-                                '\\\\usepackage[papersize={7in,7in},margin=0.3in]{geometry}', sep = '\n'))
+          documentclass = paste('\\documentclass{article}',
+                                '\\usepackage[papersize={7in,7in},margin=0.3in]{geometry}', sep = '\n'))
 
 ## the PDF graphics output is often too large because it is uncompressed; try the option ani.options('pdftk') or ani.options('qpdf') to compress the PDF graphics; see ?pdftk or ?qpdf and ?ani.options
