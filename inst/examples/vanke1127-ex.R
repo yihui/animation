d327518506fabd242dfbdf9fa655207b8914b691
root <yulj2010@gmail.com>
2015-07-12 20:30:39 +0800

    add output filename

--- inst/examples/vanke1127-ex.R
+++ inst/examples/vanke1127-ex.R
@@ -7,15 +7,15 @@ oopt = ani.options(interval = 0.5, loop = FALSE, title='Stock price of Vanke')
 ## a series of HTML animations with different time spans
 saveHTML({
   price.ani(vanke1127$price, vanke1127$time, lwd = 2)
-}, img.name = 'vanke_a', description = 'Prices changing along with time interval 15 min')
+}, img.name = 'vanke_a', description = 'Prices changing along with time interval 15 min',htmlfile = "vanke1127_1.html")
 
 saveHTML({
   price.ani(vanke1127$price, vanke1127$time, span = 30 * 60, lwd = 3)
-}, img.name='vanke_b', description = 'Prices changing along with time interval 30 min')
+}, img.name='vanke_b', description = 'Prices changing along with time interval 30 min',htmlfile = "vanke1127_2.html")
 
 saveHTML({
   price.ani(vanke1127$price, vanke1127$time, span = 5 * 60, lwd = 2)
-}, img.name='vanke_c', description = 'Prices changing along with time interval 5 min')
+}, img.name='vanke_c', description = 'Prices changing along with time interval 5 min',htmlfile = "vanke1127_3.html")
 
 ## GIF animation
 saveGIF(price.ani(vanke1127$price, vanke1127$time, lwd = 2),
